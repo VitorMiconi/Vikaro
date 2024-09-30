cafd7ae7530c95c1221083baefb67360c8602961
Kauã Gomes <kauaoficialkr@outlook.com>
2024-09-30 17:23:31 -0300

    fonte no botao

--- vikaro/src/components/BorderButton/index.js
+++ vikaro/src/components/BorderButton/index.js
@@ -1,6 +1,6 @@
 const BorderButton = (props) => {
     return (
-        <button className='bg-none border-solid border-white border text-white py-1 px-5'>
+        <button className='bg-none border-solid border-white border text-white py-1 px-5 font-lemon font-light'>
             {props.children}
         </button>
     )
