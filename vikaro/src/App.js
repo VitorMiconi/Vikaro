85d29a49758e2c233cab86b597f452a730348217
Vitor Miconi <163423734+VitorMiconi@users.noreply.github.com>
2024-09-30 22:41:37 -0300

    ajustes

--- vikaro/src/App.js
+++ vikaro/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import MainSection from "./components/MainSection";
+import BigVikaro from "./components/BigVikaro";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <MainSection></MainSection>
+      <BigVikaro></BigVikaro>
     </div>
   );
 }
