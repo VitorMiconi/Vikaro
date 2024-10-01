02ecd0f04b06a7a70b70e65904ec1c8d82735490
Vitor Miconi <163423734+VitorMiconi@users.noreply.github.com>
2024-09-30 23:16:00 -0300

    About

---
+++ vikaro/src/components/AboutSection/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const AboutSection = () => {
+    return (
+        <div className='flex items-start justify-center  relative'>
+            <p className='text-white font-light w-[60%] flex justify-center pt-14 pb-32'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            Why do we use it?
+            <br></br><br></br>
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). 
+            <br></br><br></br>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            Why do we use it?
+            </p>
+            <img className='absolute bottom-0 right-[20%]' src="/img/signature.svg" alt="Vikaro Star"></img>  
+            
+        </div>
+    )
+}
+
+export default AboutSection;
