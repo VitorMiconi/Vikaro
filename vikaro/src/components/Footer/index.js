de15c57297437c8f72a116eec71139b485162b5e
Kauã Gomes <kauaoficialkr@outlook.com>
2024-10-02 19:49:50 -0300

    IMPORTANTE DO MOBILE PRONTO

--- vikaro/src/components/Footer/index.js
+++ vikaro/src/components/Footer/index.js
@@ -1,12 +1,12 @@
 const Footer = () => {
     return (
-        <div className="font-lemon font-light text-white">
+        <div className="font-lemon font-light text-white flex flex-col gap-6">
             <div className="text-center w-full relative pt-6">
-                <h2 className="text-4xl absolute left-2/4 transform -translate-x-1/2">VIKARO</h2>
+                <h2 className="text-4xl block sm:absolute sm:left-2/4 sm:transform sm:-translate-x-1/2">VIKARO</h2>
             </div>
-            <div className="w-full flex justify-between items-end px-6 pb-8">
+            <div className="w-full flex flex-col sm:flex-row justify-between sm:items-end px-6 py-8 sm:py-0 sm:pb-8 gap-6">
                 <div>
-                    <ul className="flex flex-col gap-2">
+                    <ul className="flex flex-col gap-2 items-start">
                         <a href="#tag"><li>VOLTAR AO TOPO</li></a>
                         <a href="#tag"><li>PROJETOS</li></a>
                         <a href="#tag"><li>SERVIÇOS</li></a>
@@ -14,7 +14,7 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div>
-                    <ul className="flex flex-col gap-2">
+                    <ul className="flex flex-col gap-2 items-start">
                         <a href="#tag"><li>(11) 98789-9080</li></a>
                         <a href="#tag"><li>VIKAROAGENCIA@GMAIL.COM</li></a>
                     </ul>
--- vikaro/src/components/ProjectSquare/index.js
+++ vikaro/src/components/ProjectSquare/index.js
@@ -3,7 +3,13 @@ import './ProjectSquare.css';
 const ProjectSquare = ({ backgroundClass, children }) => {
     return (
         <div className={`w-full cursor-pointer ${backgroundClass} bg-no-repeat bg-cover h-[50vh] relative transition-all duration-500 group`}>
-            <div className="absolute top-0 left-0 p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-500 text-white text-xl">
+            <div className="p-4 text-white text-xl block sm:hidden">
+                <div className='flex flex-col gap-1'>
+                    <h1 className="text-4xl font-medium">{children}</h1>
+                    <p className='font-lemon font-light mx-1 text-sm'>Clique para ver o projeto</p>
+                </div>
+            </div>
+            <div className="absolute top-0 left-0 p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-500 text-white text-xl hidden sm:block">
                 <div className='flex flex-col gap-1'>
                     <h1 className="text-8xl font-medium">{children}</h1>
                     <p className='font-lemon font-light mx-1'>Clique para ver o projeto</p>
